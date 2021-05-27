28bb9df88d587a2e478b9148b853892f2501cb2e
Sebushka89 <sebastian.rizzo@hotmail.it>
2021-05-27 13:21:19 +0200

    messo in inglese le var

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-var nome = prompt('Inserisci il tuo nome');
+var name = prompt('Inserisci il tuo nome');
 var surname = prompt('Inserisci il tuo cognome');
 var color = prompt('Inserisci il tuo colore preferito');
 
-document.getElementById('password').innerHTML = "La tua nuova password è " + nome + surname + color + 21;
+document.getElementById('password').innerHTML = "La tua nuova password è " + name + surname + color + 21;
