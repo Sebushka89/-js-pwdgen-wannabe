1dc219fd1a2197a97741a5addd77e73591359725
Sebushka89 <sebastian.rizzo@hotmail.it>
2021-05-27 13:23:45 +0200

    messo in inglese le variabili

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-var name = prompt('Inserisci il tuo nome');
+var firstName = prompt('Inserisci il tuo nome');
 var surname = prompt('Inserisci il tuo cognome');
 var color = prompt('Inserisci il tuo colore preferito');
 
-document.getElementById('password').innerHTML = "La tua nuova password è " + name + surname + color + 21;
+document.getElementById('password').innerHTML = "La tua nuova password è " + firstName + surname + color + 21;
