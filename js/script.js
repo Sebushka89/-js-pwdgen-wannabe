d09a07d4b1f09204dbe46a95197b55ff56dfdc21
Sebushka89 <sebastian.rizzo@hotmail.it>
2021-05-27 13:08:10 +0200

    Password Generetor

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var nome = prompt('Inserisci il tuo nome');
+var surname = prompt('Inserisci il tuo cognome');
+var color = prompt('Inserisci il tuo colore preferito');
+
+document.getElementById('password').innerHTML = "La tua nuova password è " + nome + surname + color + 21;
